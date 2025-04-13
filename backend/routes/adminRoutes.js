6bdab80a6e6a4c00c26118473f16025ddce56a0c
Amit <amore43035@gmail.com>
2025-04-13 12:43:32 +0530

    Auth done

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -6,9 +6,9 @@ const { authenticateAdmin } = require("../middlewares/authMiddleware");
 router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
 
-// Example protected route
-// router.get("/dashboard", authenticateAdmin, (req, res) => {
-//   res.json({ message: `Welcome Admin ${req.user.name}` });
-// });
+
+router.get("/dashboard", authenticateAdmin, (req, res) => {
+  res.json({ message: `Welcome Admin ${req.user.name}` });
+});
 
 module.exports = router;
