a746d5fc3c9a816841d894f17f1620a5b936d7ba
vrushmore123 <132755812+vrushmore123@users.noreply.github.com>
2025-04-13 13:04:14 +0530

    setting up home page and visuals

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,21 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Visuals from "./pages/Visuals";
 import Cultural from "./pages/Cultural";
 import Meeting from "./pages/Meeting";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Welcome to the Project</h1>
-        <div className="card-container">
-          <div
-            className="card"
-            onClick={() => (window.location.href = "/visuals")}
-          >
-            <h2>Visuals</h2>
-            <p>Handwritten letters, poems, live concerts</p>
-          </div>
-          <div
-            className="card"
-            onClick={() => (window.location.href = "/cultural")}
-          >
-            <h2>Cultural</h2>
-            <p>Images, cultural events, videos</p>
-          </div>
-          <div
-            className="card"
-            onClick={() => (window.location.href = "/meeting")}
-          >
-            <h2>Meeting</h2>
-            <p>Depression, anxiety, etc.</p>
-          </div>
-        </div>
         <Routes>
-          <Route path="/visuals" component={Visuals} />
-          <Route path="/cultural" component={Cultural} />
-          <Route path="/meeting" component={Meeting} />
+          <Route path="/" element={<Home />} />
+          <Route path="/visuals" element={<Visuals />} />
+          <Route path="/cultural" element={<Cultural />} />
+          <Route path="/meeting" element={<Meeting />} />
         </Routes>
       </div>
     </Router>
